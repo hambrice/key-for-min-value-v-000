8115d60bb017a78a75d107aff3c6124ff3b39643
Brad Hambrice <hambrice@uab.edu>
2018-01-05 23:55:30 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -18,7 +18,9 @@ def key_for_min_value(name_hash)
   x = nil 
   array = [name_hash.collect {|key,value| value}]
   if name_hash.each do |key, value|
-    value < x 
+    value < x
+  end
+  value = x
     if array.all? do |number|
        number >= x
      end
