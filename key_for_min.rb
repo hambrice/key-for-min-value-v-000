33ea26a0fba0d4b10290098e9011cf151a1cb71d
Brad Hambrice <hambrice@uab.edu>
2018-01-05 23:03:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-hash = [value1: 2, value2: 3, value3: 6]
+hash = {value1: 2, value2: 3, value3: 6}
 array = []
- hash.collect {|key,value| array << value}
+ array = [hash.collect {|key,value| array << value}]
 
 puts array 
