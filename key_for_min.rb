0c2c000d4bc679230c23fb2bcc3576b1d03aa303
Brad Hambrice <hambrice@uab.edu>
2018-01-05 23:03:35 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 hash = {value1: 2, value2: 3, value3: 6}
-array = []
  array = [hash.collect {|key,value| value}]
 
 puts array 
